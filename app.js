97ec9b850fa2e48127133f84fee517e25fa7afcb
Afiq <k.afiq.azman@gmail.com>
2022-07-19 18:18:59 +0800

    Setting up app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "production"){
     app.use(express.static('public'));
     app.get('*',(req, res)=>{
         //req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-        res.sendFile('index.html', { root: __dirname })
+        res.sendFile('src/index.html', { root: __dirname })
     })
 }
 
