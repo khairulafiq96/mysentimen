2f24893f76676b81752a80ec7c14799806e9ca7a
Afiq <k.afiq.azman@gmail.com>
2022-07-19 18:16:29 +0800

    Setting up app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 //Install express server
 const express = require('express');
-var path = require('path');
+const path = require('path');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production"){
-    app.use(express.static('build'));
+    app.use(express.static('public'));
     app.get('*',(req, res)=>{
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        //req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile('index.html', { root: __dirname })
     })
 }
 
